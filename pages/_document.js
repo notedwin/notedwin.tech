a5a12258f240b21bee3fa6a4799fde0089ed9263
Edwin Zamudio <ezamudio@hawk.iit.edu>
2022-06-21 21:16:41 -0500

    feat: umami

--- pages/_document.js
+++ pages/_document.js
@@ -4,10 +4,13 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+        <script async defer data-website-id="556351d8-60ef-4d44-bd2d-8797dce107b9" src="https://umami.notedwin.tech/umami.js"></script>
+        </Head>
         <body>
           <Main />
           <NextScript />
+
         </body>
       </Html>
     )
