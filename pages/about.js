47118e7521126b3b48ec6ad23ba72f25c4163e95
Edwin Zamudio <zamudio.e13@gmail.com>
2024-03-09 16:23:47 -0600

    update work + add some draft posts

--- pages/about.js
+++ pages/about.js
@@ -3,57 +3,54 @@ import Navbar from "../components/navbar";
 import Meta from "../components/meta";
 
 export default function ResumePage() {
-  return (
-    <>
-      <Meta />
-      <Head>
-        <title>Edwin Zamudio</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Navbar />
-      <article>
-        <h1>About Me</h1>
-        <div className="about">
-          <p>Hey there!</p>
-          <p>
-            I'm Edwin, a Software Engineer based in Chicago. I'm currently
-            working at Deloitte as a Data Engineer. I'm interested in building efficient backend systems, and data pipelines.
-          </p>
-          In my free time, I enjoy playing soccer, biking and learning new things, like building small data systems like <a href="https://map.notedwin.com"> this </a>.
-          <p>
-            I previously worked at Cox Automotive where I created data pipelines
-            to create alerts for cost and security issues using Python,
-            Terraform, and AWS. Previous jobs include being a software developer
-            for Illinois Tech's Idea Shop, and a software engineer for HaxML, an
-            online sports Analytics platform.
-          </p>
-          <p>Technical Areas of Interests</p>
-          <ul>
-            <li>Backend Systems</li>
-            <li>Data Pipelines</li>
-            <li>Cloud Computing</li>
-            <li>Machine Learning</li>
-            <li>Software Engineering</li>
-          </ul>
-
-          <p>Technical Skills</p>
-          <ul>
-            <li>Python</li>
-            <li>Rust</li>
-            <li>Amazon Web Services</li>
-            <li>SQL</li>
-          </ul>
-          <p>
-            You can
-            <a href="http://github.com/notedwin/notedwin.tech/issues/new"> report </a>
-            if there is a broken link(s) or something else.
-          </p>
-          <h3>This Website</h3>
-          <p>
-            This website is built with Next.js, and hosted on Vercel. However, this website is very simple as most pages are markdown files.
-          </p>
-        </div>
-      </article>
-    </>
-  );
+	return (
+		<>
+			<Meta />
+			<Head>
+				<title>Edwin Zamudio</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Navbar />
+			<article>
+				<h1>About Me</h1>
+				<div className="about">
+					<p>Hey there!</p>
+					<p>I'm Edwin, a Data Engineer based in Chicago.</p>
+					<p>
+						I'm currently working at Amgen as a Data Engineer. I have an
+						interest in building impactful data systems like{" "}
+						<a href="https://map.notedwin.com"> this. </a>
+					</p>
+					<p>
+						In my free time, I enjoy playing soccer, biking and learning new
+						things, failing leetcode mediums, and turning electricity into heat.
+					</p>
+					<h2>Technical Areas of Interests</h2>
+					<ul>
+						<li>Backend Systems</li>
+						<li>Data Systems</li>
+						<li>Cloud Computing</li>
+						<li>Using duckdb as a hammer</li>
+					</ul>
+					<h2>Technical Skills</h2>
+					<ul>
+						<li>Languages: Python, SQL</li>
+						<li>Cloud Platforms: AWS, GCP</li>
+						<li>
+							Other keywords: pyspark, duckdb, dagster, ansible, terraform,
+							linux
+						</li>
+					</ul>
+					<p>
+						You can
+						<a href="http://github.com/notedwin/notedwin.tech/issues/new">
+							{" "}
+							report{" "}
+						</a>
+						if there is a broken link(s) or something else.
+					</p>
+				</div>
+			</article>
+		</>
+	);
 }
